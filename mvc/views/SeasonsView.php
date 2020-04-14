1283de75d19d2c51b6a93bab7da4824d75408ce3
DalBrar <brardal@hotmail.com>
2020-04-13 22:41:03 -0600

    since its an API no Models or Views were needed to be used and thus removed from previous project template.

--- mvc/views/SeasonsView.php
+++
@@ -1,6 +0,0 @@
-<?php include VIEWS.'layouts/main/header.php'; ?>
-<div>
-	
-</div>
-</body>
-</html>
--- mvc/views/layouts/Toolbar.blade.php
+++
@@ -1,7 +0,0 @@
-<div id="titlebar">
-	<div id="title">@{ APP_NAME }@</div>
-	<span id="toolbar" class="dim">
-		<a href="/tv/search">Search</a> |
-		<a class="dim" href="/tv/logout">Logout</a>
-	</span>
-</div>
